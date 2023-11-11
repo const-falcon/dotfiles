b2e5a79a71bc9342101a36251ef59677d7d91dc8
falcon <93477932+const-falcon@users.noreply.github.com>
2023-11-12 02:19:36 +0900

    ✨ Add setup script, README.

---
+++ setup.fish
@@ -0,0 +1,14 @@
+date
+echo "===== setup start. ====="
+
+ln -sf ~/.config/fish ~/dotfiles/fish
+ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/vim/.vim ~/.vim
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
+
+# iTerm2 の設定を反映
+# @link https://qiita.com/cakipy/items/2e5d03d5e063c6734b34
+# sudo killall cfprefsd
+
+date
+echo "===== setup done. ====="
